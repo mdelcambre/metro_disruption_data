15bf2fb543311011f5e44f8db5b3f3cc6fcd4cd3
Mark Delcambre <mark@delcambre.com>
2016-06-20 14:53:36 -0400

    untested attempt to write a parser

--- src/parse.py
+++ src/parse.py
@@ -10,15 +10,20 @@ __email__ = "mark@delcambre.com"
 
 # built-in
 from argparse import Namespace
+import re
 
 # regex
 regex_group = Namespace(
-    time        = r'^(\d{1,2}:\d\d (?:a\.m\.|p\.m\.))',
-    direction   = r'
-
-
-def parse_dsrp(text):
-    
-    
+    time        = re.compile(r'^(\d{1,2}:\d\d (?:a\.m\.|p\.m\.))'),
+    direction   = re.compile(r'A (\w+?)-bound'),
+    color       = re.compile(r'bound (\w+?) Line train'),
+    station     = re.compile(r'train at (\w+?) (?:was|had)'),
+    formats     = (
+        re.compile(r'(?P<reason>did not operate), resulting in a (?P<delay>\d+?)-minute')
+        re.compile(r'was (?P<reason> was expressed for .+)')
+        re.compile(r'as delayed (?P<delay>\d+?) minutes? due to (?P<reason>.+)')
+        re.compile(r'was offloaded due to (?P<reason>[^.]+)\. Passengers experienced a (?P<delay>\d+?)-minute')
+    )
+)
 
 
